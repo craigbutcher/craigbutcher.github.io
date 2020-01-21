76785c216fc92d2fcd36f9acabfd375ce6eca905
Craig Butcher <craigbutcher@users.noreply.github.com>
2020-01-21 10:16:35 +0000

    Added inclusive language plugin

--- .eleventy.js
+++ .eleventy.js
@@ -5,12 +5,17 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
+const inclusiveLangPlugin = require("@11ty/eleventy-plugin-inclusive-language");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
-
+  eleventyConfig.addPlugin(inclusiveLangPlugin, {
+    templateFormats: ["md"],
+    words: "simply,obviously,basically,of course,clearly,just,everyone knows,however,easy"
+  });
+  
   eleventyConfig.setDataDeepMerge(true);
   eleventyConfig.addLayoutAlias("post", "./layouts/post.njk");
   eleventyConfig.addFilter("readableDate", dateObj => {
